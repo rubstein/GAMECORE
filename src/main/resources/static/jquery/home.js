0d250af2478008387ae4c5815b38dececa73275e
rubstein <rubenalejandro.most@gmail.com>
2025-03-21 13:43:13 +0100

    lista comentada

---
+++ src/main/resources/static/jquery/home.js
@@ -0,0 +1,11 @@
+$('li').on('mouseover', function(){
+    $(this).css({
+        'transform': 'scale(1.1)',
+        'transition': '0.5s'
+    })
+}).on('mouseout', function(){
+    $(this).css({
+        'transform': 'scale(1)',
+        'transition': '0.5s'
+    })
+})
