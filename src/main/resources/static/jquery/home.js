7bf658f262779ebb31cca3a271804f648b7da37c
rubstein <rubenalejandro.most@gmail.com>
2025-04-11 13:17:15 +0200

    navigation bar perfecto

--- src/main/resources/static/jquery/home.js
+++ src/main/resources/static/jquery/home.js
@@ -1,27 +1,3 @@
-$('a').on('mouseover', function(){
-    $(this).css({
-        'transform': 'scale(1.1)',
-        'transition': '0.5s'
-    })
-}).on('mouseout', function(){
-    $(this).css({
-        'transform': 'scale(1)',
-        'transition': '0.5s'
-    })
-});
 
-$('.negro').on('mouseover', function(){
-    $(this).css('color', 'black');
-});
-$('.verde').on('mouseover', function(){
-    $(this).css('color', 'green');
-}).on('mouseout', function(){
-    $(this).css('color', 'black');
-});
-$('.rojo').on('mouseover', function(){
-    $(this).css('color', 'red');
-}).on('mouseout', function(){
-    $(this).css('color', 'black');
-});
 
 
