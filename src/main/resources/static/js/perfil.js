5a1814d1145bf6473434b89368b2fdfe885d2244
rubstein <rubenalejandro.most@gmail.com>
2025-04-21 10:25:41 +0200

    edición datos usuario

---
+++ src/main/resources/static/js/perfil.js
@@ -0,0 +1,37 @@
+function activarEdicion(){
+    var valorOriginal = document.getElementById("editar").value;
+    var idOriginal = document.getElementById("editar").id;
+
+    var botonGuardar = document.createElement("button");
+    botonGuardar.innerHTML = "Guardar Cambios";
+    botonGuardar.id ="guardarCambios";
+    botonGuardar.className = "boton";
+
+    document.getElementById("nuevoNombreUsuario").disabled = false;
+    document.getElementById("nuevoEmail").disabled = false;
+    
+    document.getElementById("editar").value = "Cancelar"
+    document.getElementById("editar").id = "cancelar";
+    
+    
+    if (!document.getElementById("guardarCambios")) {
+        document.getElementById("formulario").appendChild(botonGuardar);
+    }
+
+    document.getElementById("cancelar").onclick = function(){
+        document.getElementById("nuevoNombreUsuario").disabled = true;
+        document.getElementById("nuevoEmail").disabled = true;
+
+        if (document.getElementById("guardarCambios")) {
+            document.getElementById("formulario").removeChild(document.getElementById("guardarCambios"));
+        }
+
+        document.getElementById("cancelar").value = valorOriginal;
+        document.getElementById("cancelar").id = idOriginal; 
+        document.getElementById("editar").onclick = activarEdicion;
+    }
+}
+
+document.getElementById("editar").onclick = function(){
+    activarEdicion();
+}
